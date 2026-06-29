5da2540881224e6404464329a4e67f465a964fcc
hgritty_wsl <hgritty123@gamil.com>
2026-06-29 20:59:27 +0800

    Init commit

---
+++ library.cpp
@@ -0,0 +1,21 @@
+// Library Management System
+// Author : hgritty
+// Date : 2026-6-29
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+int main() {
+    return 0;
+}
+
+void addBook();
+void borrowBook();
+void returnBook();
+void listBooks();
+void saveData();
+void loadData();
